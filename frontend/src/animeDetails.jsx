17cd9686a4e685ab349a010ff1fea303eb74d07e
Ben G Vuong <ben.vuong2000@gmail.com>
2024-02-11 16:05:42 -0600

    currently console log key and values of the individual entries from anime list

--- frontend/src/animeDetails.jsx
+++ frontend/src/animeDetails.jsx
@@ -4,13 +4,18 @@ function AnimeDetails(){
     const [animeList, setAnimeList] = useState([]);
     const [animeName, setAnimeName] = useState();
     let a = ""
+ 
     useEffect(() => {
         const storedAnime = JSON.parse(localStorage.getItem('anime'));
         if (storedAnime) {
             setAnimeList(storedAnime);
-             a = animeList.find((person=>person.name==="Lucky Star"))
+             a = animeList.find((person=>person.name==="Eureka 7"))
             console.log(a)
             setAnimeName(JSON.stringify(a))
+            for(const[key, value] of Object.entries(a)){
+                console.log(`${key}: ${value}`);
+                //map.set(key,value)
+            }
             
         }
 
@@ -19,8 +24,8 @@ function AnimeDetails(){
         
     return(
         <div> 
-          
-            {animeName}
+           
+            
             
            
         </div>
