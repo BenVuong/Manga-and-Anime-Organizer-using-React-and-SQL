4dbf8d06bda6ad8023489ed953d29de774029093
Ben G Vuong <78512101+BenVuong@users.noreply.github.com>
2024-07-16 18:42:09 -0500

    centered items in cards in anime list page

--- frontend/src/components/anime/animeListCards.jsx
+++ frontend/src/components/anime/animeListCards.jsx
@@ -6,10 +6,8 @@ import {
   Typography,
   Grid,
   Card,
-  TextField,
   CardContent,
   CardHeader,
-  Button,
 } from "@mui/material";
 export function AnimeListCards({
   data,
@@ -32,76 +30,82 @@ export function AnimeListCards({
                   <Grid container alignContent="center" justifyContent="center">
                     <CardContent>
                       <CardHeader
+                        style={{ textAlign: "center" }}
                         title={anime.title}
                         subheader={"Status: " + anime.status}
                       ></CardHeader>
-                      <Grid item alignItems="center" justifyContent="center">
+                      <Grid item style={{ textAlign: "center" }}>
                         Score: {anime.score}
                       </Grid>
-                      <Grid item alignItems="center" justifyContent="center">
+                      <Typography style={{ textAlign: "center" }}>
                         <img
                           src={anime.image}
-                          style={{ width: "auto", height: "256px" }}
+                          alt="Cover art"
+                          style={{
+                            width: "auto",
+                            height: "256px",
+                            alignContent: "center",
+                          }}
                         ></img>
-                      </Grid>
-                      <Grid item>
+                      </Typography>
+                      <Typography style={{ textAlign: "center" }}>
                         Watch Progress {anime.episodesWatched}/
                         {anime.episodeCount}
-                      </Grid>
-                      <Grid item>
-                        <Link
-                          className="btn btn-info "
-                          role="button"
-                          to={`/readanime/${anime.id}`}
-                        >
-                          Details
-                        </Link>
+                        <Grid item>
+                          <Link
+                            className="btn btn-info "
+                            role="button"
+                            to={`/readanime/${anime.id}`}
+                          >
+                            Details
+                          </Link>
 
-                        <button
-                          onClick={() => handleOpen(anime.title, anime.id)}
-                          className="btn btn-danger"
-                        >
-                          {""}
-                          Delete
-                        </button>
-                        <Modal
-                          open={open}
-                          onClose={handleClose}
-                          aria-labelledby="modal-modal-title"
-                          aria-describedby="modal-modal-description"
-                        >
-                          <Box sx={style}>
-                            <Typography
-                              id="modal-modal-title"
-                              variant="h6"
-                              component="h2"
-                            >
-                              Are you sure you want to delete {animeName} from
-                              your list?
-                            </Typography>
-                            <Typography
-                              id="modal-modal-description"
-                              sx={{ mt: 2 }}
-                            >
-                              <button
-                                onClick={() => handleDelete(animeID)}
-                                className="btn btn-danger"
+                          <button
+                            onClick={() => handleOpen(anime.title, anime.id)}
+                            className="btn btn-danger"
+                          >
+                            {""}
+                            Delete
+                          </button>
+                          <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                          >
+                            <Box sx={style}>
+                              <Typography
+                                id="modal-modal-title"
+                                variant="h6"
+                                component="h2"
                               >
-                                {" "}
-                                Yes I am Sure
-                              </button>
+                                Are you sure you want to delete {animeName} from
+                                your list?
+                              </Typography>
+                              <Typography
+                                id="modal-modal-description"
+                                sx={{ mt: 2 }}
+                              >
+                                <button
+                                  onClick={() => handleDelete(animeID)}
+                                  className="btn btn-danger"
+                                >
+                                  {" "}
+                                  Yes I am Sure
+                                </button>
 
-                              <button
-                                onClick={() => window.location.reload()}
-                                className="btn btn-success"
-                              >
-                                {" "}
-                                No
-                              </button>
-                            </Typography>
-                          </Box>
-                        </Modal>
-                      </Grid>
+                                <button
+                                  onClick={() => window.location.reload()}
+                                  className="btn btn-success"
+                                >
+                                  {" "}
+                                  No
+                                </button>
+                              </Typography>
+                            </Box>
+                          </Modal>
+                        </Grid>
+                      </Typography>
                     </CardContent>
                   </Grid>
                 </Card>
